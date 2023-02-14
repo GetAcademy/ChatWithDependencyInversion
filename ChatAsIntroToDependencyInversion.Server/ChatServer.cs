49587be84fe9930598498e310418e7428b31ee75
GET Academy <terje@getacademy.no>
2023-02-14 10:28:38 +0100

    Initial commit

---
+++ ChatAsIntroToDependencyInversion.Server/ChatServer.cs
@@ -0,0 +1,30 @@
+﻿using ChatAsIntroToDependencyInversion.Server;
+
+namespace ChatAsIntroToDependencyInversion
+{
+    public class ChatServer
+    {
+        readonly List<IChatClient> _clients;
+
+        public ChatServer()
+        {
+            _clients = new List<IChatClient>();
+        }
+
+        public void Broadcast(IChatClient client, string message)
+        {
+            foreach (var chatClient in _clients)
+            {
+                if (chatClient != client)
+                {
+                    chatClient.Receive(message);
+                }
+            }
+        }
+
+        public void Register(IChatClient chatClient)
+        {
+            _clients.Add(chatClient);
+        }
+    }
+}
---
+++ ChatAsIntroToDependencyInversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using ChatAsIntroToDependencyInversion;
+
+/*
+ * Eks 1 - chat: Dependency Inversion Principle
+ *
+ * Eks 2 - søke i filer - Single Responsibility Principle og Dependency Injection (teknikk)
+ *
+ *
+ * Pause til 10:30
+ */
+
+
+var server = new ChatServer();
+
+var client1 = new ChatClient("Per", server);
+var client2 = new ChatClient("Pål", server);
+var client3 = new ChatClient("Espen", server);
+
+client1.Say("Halla");
+// Pål og Espen får beskjed "Per sier Halla"
+
+client2.Say("Helle");
+// Per og Espen får beskjed "Pål sier Helle"
+
+client3.Say("Hollo");
+// Per og Pål får beskjed "Espen sier Hollo"
